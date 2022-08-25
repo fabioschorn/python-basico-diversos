8b6a514382b67c1d8be715890515b5ece0a0269d
FSchorn <fschorn@gmail.com>
2022-08-24 22:17:14 -0300

    Add bigdigits.

---
+++ examples/bigdigits/bigdigits.py
@@ -0,0 +1,31 @@
+import sys
+ 
+Zero  = [" *** ", "*   *", "*   *", "*   *", "*   *", "*   *", " *** "]
+One   = [" * ", "** ", " * ", " * ", " * ", " * ", "***"]
+Two   = [" *** ", "*   *", "*  * ", "  *  ", " *   ", "*    ", "*****"]
+Three = [" *** ", "*   *", "    *", "  ** ", "    *", "*   *", " *** "]
+Four  = ["    * ", "   ** ", "  * * ", " *  * ", "******", "    * ", "    * "]
+Five  = ["*****", "*    ", "*    ", " *** ", "    *", "*   *", " *** "]
+Six   = [" *** ", "*   *", "*    ", "**** ", "*   *", "*   *", " *** "]
+Seven = ["*****", "    *", "   * ", "  *  ", " *   ", "*    ", "*    "]
+Eight = [" *** ", "*   *", "*   *", " *** ", "*   *", "*   *", " *** "]
+Nine  = [" ****", "*   *", "*   *", " ****", "    *", "    *", " *** "]
+ 
+Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+
+try:
+    digits = sys.argv[1]
+    row = 0
+    while row < 7:
+        line = ""
+        column = 0
+        while column < len(digits) :
+            number = int(digits[column])
+            line += Digits[number][row] + " "
+            column += 1
+        print(line)
+        row += 1
+except IndexError:
+    print("usage: bigdigits.py <number>")
+except ValueError as err:
+    print(err, "in", digits)
