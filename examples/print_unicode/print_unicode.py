e096e4edf7f132edef2f86a1a4bb42f8269d1457
FSchorn <fschorn@gmail.com>
2022-10-15 19:07:55 -0300

    Adding comments and spaces.

--- examples/print_unicode/print_unicode.py
+++ examples/print_unicode/print_unicode.py
@@ -1,18 +1,21 @@
 #Print Unicode characters.
 import unicodedata
 import sys
+
+# Deffine the header of the program
 def print_unicode_table(word):
     print("decimal hex chr {0:^40}".format("name"))
     print("------- ----- --- {0:-<40}".format(""))
     code = ord(" ")
     end = sys.maxunicode
+# While the code is less than the end of the unicode table
     while code < end:
         c = chr(code)
         name = unicodedata.name(c, "*** unknown ***")
         if word is None or word in name.lower():
             print("{0:7} {0:5X} {0:^3c} {1}".format(code, name.title()))
         code += 1
-
+# If the program is called from the command line and the word is not empty
 if __name__ == "__main__":
     word = None
     if len(sys.argv) > 1:
@@ -21,5 +24,6 @@ if __name__ == "__main__":
             word = 0
         else:
             word = sys.argv[1].lower()
+# If the word is not empty
     if word != 0:
         print_unicode_table(word)
