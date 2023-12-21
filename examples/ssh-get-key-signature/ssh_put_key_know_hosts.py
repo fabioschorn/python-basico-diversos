a45520fcd50b53c0e83bafaa34c70acfe4a511d4
FSchorn <fschorn@gmail.com>
2023-12-20 21:28:20 -0300

    Changing...

--- examples/ssh-get-key-signature/ssh_put_key_know_hosts.py
+++ examples/ssh-get-key-signature/ssh_put_key_know_hosts.py
@@ -8,26 +8,44 @@ def get_ip_addresses(file_path):
     with open(file_path, 'r') as file:
         return [line.strip() for line in file.readlines()]
 
+def is_ip_in_known_hosts(ip_address, known_hosts_file):
+    """Check if the IP address is already in the known_hosts file."""
+    try:
+        with open(known_hosts_file, 'r') as file:
+            for line in file:
+                if ip_address in line:
+                    return True
+        return False
+    except FileNotFoundError:
+        # If the file doesn't exist, treat it as if the IP is not in it.
+        return False
+
 def add_to_known_hosts(ip_address):
     """Add the SSH key signature of the given IP address to known_hosts."""
+    known_hosts_file = os.path.expanduser("~/.ssh/known_hosts")
+
+    if is_ip_in_known_hosts(ip_address, known_hosts_file):
+        print(f"{ip_address} is already in known_hosts.")
+        return
+
     try:
         ssh_keyscan_command = ["ssh-keyscan", ip_address]
         ssh_output = subprocess.check_output(ssh_keyscan_command, stderr=subprocess.STDOUT).decode()
 
-        known_hosts_file = os.path.expanduser("~/.ssh/known_hosts")
         with open(known_hosts_file, "a") as known_hosts:
             known_hosts.write(ssh_output)
+        print(f"Added {ip_address} to known_hosts.")
+
     except subprocess.CalledProcessError as e:
-        print(f"Error occurred while processing {ip_address}: {e.output.decode()}")
+        print(f"Error adding {ip_address} to known_hosts.")
+        print(e.output.decode())
 
 def main():
-    hosts_file = 'hosts'
-    ip_addresses = get_ip_addresses(hosts_file)
+    ip_addresses = get_ip_addresses(file_path)
 
     for ip in ip_addresses:
         print(f"Processing {ip}...")
         add_to_known_hosts(ip)
-        print(f"Added {ip} to known_hosts.")
 
 if __name__ == "__main__":
     main()
