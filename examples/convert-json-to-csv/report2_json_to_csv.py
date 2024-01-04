04a18152ff9da215f9782d6df833eb635d892624
FSchorn <fschorn@gmail.com>
2024-01-04 10:21:47 -0300

    Update python!

--- examples/convert-json-to-csv/report2_json_to_csv.py
+++ examples/convert-json-to-csv/report2_json_to_csv.py
@@ -8,20 +8,24 @@ json_directory = '/path/to/json/files'
 output_csv = '/path/to/output/cloud_report.csv'
 dataframes = []
 
+# Iterate over each file in the directory
 for filename in os.listdir(json_directory):
     if filename.endswith('.json'):
         file_path = os.path.join(json_directory, filename)
         with open(file_path) as json_file:
             data = json.load(json_file)
-            # Convert data to DataFrame
+            # Convert the JSON data to a DataFrame
             df = pd.DataFrame(data)
-            # Add a new column for the filename
-            df['source_file'] = filename
-            # Append the DataFrame to the list
+            # Remove the '.json' extension from the filename
+            filename_without_extension = os.path.splitext(filename)[0]
+            # Add a column with the filename without the extension
+            df['aws_account_number'] = filename_without_extension
+            # Append the DataFrame to the list of DataFrames
             dataframes.append(df)
 
-# Combine all DataFrames
+# Combine all DataFrames into a single DataFrame
 combined_df = pd.concat(dataframes, ignore_index=True)
-# Save to CSV
+
+# Write the combined DataFrame to a CSV file
 combined_df.to_csv(output_csv, index=False)
 print("Completed", output_csv)
