85a899b9548489d7a4dfb2153278f13aa0689a88
FSchorn <fschorn@gmail.com>
2024-01-04 17:50:46 -0300

    Update7!

--- examples/convert-json-to-csv/report2_json_to_csv.py
+++ examples/convert-json-to-csv/report2_json_to_csv.py
@@ -22,6 +22,34 @@ for filename in os.listdir(json_directory):
 
 # Combine all DataFrames
 combined_df = pd.concat(dataframes, ignore_index=True)
+
 # Save to CSV
 combined_df.to_csv(output_csv, index=False)
-print("Completed", output_csv)
+print("Part 1: CSV file created!", output_csv)
+
+# Read the CSV file, remove the .json extension from the 'aws_account_number' colum's values
+df = pd.read_csv(output_csv)
+df['aws_account_number'] = df['aws_account_number'].str.replace('.json', '')
+
+# Save to CSV
+df.to_csv(output_csv, index=False)
+print("Part 2: CSV change column created!", output_csv)
+
+# Change the column order, move the 'aws_account_number' column to the first position
+cols = df.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+df = df[cols]
+
+# Save to CSV
+df.to_csv(output_csv, index=False)
+print("Part 3: CSV change column order created!", output_csv)
+
+# Convert CSV to Excel
+output_excel = '/path/to/output/cloud_report.xlsx'  # Define the path for the Excel file
+df.to_excel(output_excel, index=False)  # Save the DataFrame as an Excel file
+print("Part 4: Excel file created!", output_excel)
+
+# Delete the Original CSV File
+if os.path.exists(output_csv):
+    os.remove(output_csv)
+    print("Part 5: CSV file deleted!", output_csv)
