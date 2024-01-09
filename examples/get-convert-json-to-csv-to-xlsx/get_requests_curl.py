5c8b0dbf3dff028b1416e8194676d1880a11e542
FSchorn <fschorn@gmail.com>
2024-01-09 18:44:32 -0300

    Change code!!!

--- examples/get-convert-json-to-csv-to-xlsx/get_requests_curl.py
+++ examples/get-convert-json-to-csv-to-xlsx/get_requests_curl.py
@@ -9,20 +9,21 @@ def fetch_and_save_data(product_name, user_token, csv_file_path, output_director
             if row['productName'] == product_name:
                 cloud_account_id = row['cloudAccountId']
                 external_account_number = row['externalAccountNumber']
-                response = requests.get(
-                    f'https://api.cloudcheckr.com/api/v2/cloud/{cloud_account_id}/assessments/failures',
-                    headers={'accept': 'application/json', 'Authorization': f'Bearer {user_token}'}
-                )
-                if response.status_code == 200:
-                    with open(f'{output_directory}/{external_account_number}.json', 'w') as json_file:
-                        json.dump(response.json(), json_file)
-                else:
-                    print(f'Failed to fetch data for cloudAccountId: {cloud_account_id}')
+                url = f'https://api.cloudcheckr.com/api/v2/cloud/{cloud_account_id}/assessments/failures'
+                headers = {
+                    'accept': 'application/json',
+                    'Authorization': f'Bearer {user_token}'
+                }
+                response = requests.get(url, headers=headers)
+                json_data = response.json()
+                file_name = f'{external_account_number}.json'
+                file_path = f'{output_directory}/{file_name}'
+                with open(file_path, mode='w', encoding='utf-8') as json_file:
+                    json.dump(json_data, json_file, indent=4)
 
-# Example usage
-product_name = input("Enter the product name: ")
-user_token = input("Enter your user token: ")
-csv_file_path = 'product_list.csv'  # Update this if your CSV file is in a different directory
-output_directory = 'json_results'  # Update this if you want to save JSON files in a different directory
-
-fetch_and_save_data(product_name, user_token, csv_file_path, output_directory)
+if __name__ == '__main__':
+    product_name = input('Product Name: ')
+    user_token = input('User Token: ')
+    csv_file_path = input('CSV File Path: ')
+    output_directory = input('Output Directory: ')
+    fetch_and_save_data(product_name, user_token, csv_file_path, output_directory)
