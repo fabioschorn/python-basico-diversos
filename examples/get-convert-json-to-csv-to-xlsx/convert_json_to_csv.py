fa08b59beefa9089fb6f2217a2b76700c54fe01e
FSchorn <fschorn@gmail.com>
2024-01-09 22:21:19 -0300

    Near of the end...

--- examples/get-convert-json-to-csv-to-xlsx/convert_json_to_csv.py
+++ examples/get-convert-json-to-csv-to-xlsx/convert_json_to_csv.py
@@ -1,6 +1,18 @@
+# Python script to convert JSON to CSV
+import os
+import json
 import pandas as pd
 
-# Read the JSON file
-df = pd.read_json (r'./data.json')
-# Convert to CSV
-df.to_csv (r'./data.csv', index = None)
+# Get current working directory
+json_directory = '/path/to/json/files'
+output_csv = '/path/to/output/cloud_report.csv'
+combined_data = []
+for filename in os.listdir(json_directory):
+    if filename.endswith('json'):
+        file_path = os.path.join(json_directory, filename)
+        with open(file_path) as json_file:
+            data = json.load(json_file)
+            combined_data.extend(data)
+df = pd.DataFrame(combined_data)
+df.to_csv(output_csv, index=False)
+print("completed", output_csv)
