dbdd01cec92f2693e1430a8fd85e18bb9c55db8c
FSchorn <fschorn@gmail.com>
2024-04-21 02:06:39 -0300

    Little changes!

--- examples/get-convert-json-to-csv-to-xlsx/get_token.py
+++ examples/get-convert-json-to-csv-to-xlsx/get_token.py
@@ -1,6 +1,17 @@
 import requests
 from getpass import getpass
 import json
+import os
+import hashlib
+
+# Directory where token files are stored
+token_directory = '/YOUR-PATH'
+
+# Clean up the directory at the start of the program
+for file_name in os.listdir(token_directory):
+    file_path = os.path.join(token_directory, file_name)
+    if os.path.isfile(file_path):
+        os.remove(file_path)
 
 # URL of the API endpoint that generates the token
 token_generation_url = 'https://api.fs.com.br/token'
@@ -27,9 +38,16 @@ response = requests.post(token_generation_url, headers=headers, data=payload)
 # Check if the request was successful
 if response.status_code == 200:
     token_data = response.json()
-    # Save the token JSON to a file
-    with open('token.json', 'w') as file:
-        json.dump(token_data, file)
-    print("Token saved to token.json")
+    # Save the token JSON to a file for later use
+    with open(os.path.join(token_directory, 'token.json'), 'w') as token_file:
+        json.dump(token_data, token_file)
+
+    # Create a hash of the access token and save it to another file
+    token_hash = hashlib.sha256(token_data['access_token'].encode()).hexdigest()
+    with open(os.path.join(token_directory, 'token.file'), 'w') as hash_file:
+        hash_file.write(token_hash)
+
+    # Print the token hash
+    print(f"Your token hash is: {token_hash}")
 else:
-    print("Failed to generate token: ", response.status_code)
+    print(f"Failed to generate token: {response.text}")
